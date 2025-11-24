adc35a8ced0ee4462212de932f2725ed03e16be6
Rkings023 <Rkings@howcollege.ac.uk>
2025-11-24 09:33:45 +0000

    acccontroller

---
+++ WDPDY2_Project/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using WDPDY2_Project.Models;
+
+namespace WDPDY2_Project.Controllers
+{
+    public class AccountController : Controller
+    {
+
+
+
+    }
+}
