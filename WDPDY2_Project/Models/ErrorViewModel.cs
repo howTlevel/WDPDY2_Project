c2033593af221a4064714f7efa49558247ca7b82
Rkings023 <Rkings@howcollege.ac.uk>
2025-11-24 09:31:47 +0000

    Add project files.

---
+++ WDPDY2_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WDPDY2_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
