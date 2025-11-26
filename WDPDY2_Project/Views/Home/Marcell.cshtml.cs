d0ef8342ae5dcf39fddac503d1755da1092778e4
Marcell Flamich <S199939@howcollege.ac.uk>
2025-11-26 14:04:50 +0000

    MF homepage

---
+++ WDPDY2_Project/Views/Home/Marcell.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WDPDY2_Project.Views.Home
+{
+    public class MarcellModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
