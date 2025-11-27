a37299558a33a12b08281e60624b55463001ccab
Marcell Flamich <S199939@howcollege.ac.uk>
2025-11-27 15:58:24 +0000

    HP1 changes

--- WDPDY2_Project/Controllers/AccountController.cs
+++ WDPDY2_Project/Controllers/AccountController.cs
@@ -6,8 +6,18 @@ namespace WDPDY2_Project.Controllers
 {
     public class AccountController : Controller
     {
-
-
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
 
     }
 }
