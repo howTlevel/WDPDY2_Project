44b7ae2ad4621fcd15fcd5a78526997d69b1936c
Georgi Blazhev <S1111211@howcollege.ac.uk>
2025-11-27 15:29:59 +0000

    added georgi index page

--- WDPDY2_Project/Controllers/HomeController.cs
+++ WDPDY2_Project/Controllers/HomeController.cs
@@ -34,6 +34,10 @@ namespace WDPDY2_Project.Controllers
         {
             return View();
         }
+        public IActionResult Georgi()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
