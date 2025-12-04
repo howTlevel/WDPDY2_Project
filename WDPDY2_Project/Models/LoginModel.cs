78e20e489a7865df7df17b9a784e714ee2b095a9
Mackenzie Akehurst <S1114066@howcollege.ac.uk>
2025-12-04 15:26:02 +0000

    changed user cs to have role id

--- WDPDY2_Project/Models/LoginModel.cs
+++ WDPDY2_Project/Models/LoginModel.cs
@@ -5,8 +5,8 @@ namespace WDPDY2_Project.Models
     // LoginModel.cs
     public class LoginModel
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
         public bool RememberMe { get; set; }
     }
 
--- WDPDY2_Project/Models/User.cs
+++ WDPDY2_Project/Models/User.cs
@@ -5,6 +5,8 @@
         public int Id { get; set; }
         public string? Username { get; set; }
         public string? Password { get; set; }
-        public int RoleID { get; set; }
+
+        public string? RoleId { get; set; }
+       
     }
 }
