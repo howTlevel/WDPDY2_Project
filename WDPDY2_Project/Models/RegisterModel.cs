a52b66af8fc607ddb7cf64d77a8133a195ef4548
Marcell Flamich <S199939@howcollege.ac.uk>
2025-12-04 15:13:27 +0000

    Login, Register w/ Models

---
+++ WDPDY2_Project/Models/RegisterModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WDPDY2_Project.Models
+{
+    // RegisterModel.cs
+    public class RegisterModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+    }
+
+}
